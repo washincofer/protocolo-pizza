ca6c5ccffa630db8920e16f0cc688e7f3d50e178
washincofer <washincofer@gmail.com>
2026-09-11 09:56:28 -0300

    Restaura SceneRouter sem bloqueio de transição

--- godot/scripts/scene_router.gd
+++ godot/scripts/scene_router.gd
@@ -3,13 +3,6 @@ extends Node
 signal area_changed(area_id: String)
 
 func route_to(area_id: String) -> void:
-	if area_id.is_empty():
-		return
-	if SceneTransition.is_transitioning():
-		return
-	SceneTransition.transition_to(area_id, Callable(self, "_commit_route"))
-
-func _commit_route(area_id: String) -> void:
 	GameState.current_area = area_id
 	GameState.changed.emit()
 	area_changed.emit(area_id)
